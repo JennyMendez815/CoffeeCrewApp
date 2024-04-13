074bab25c37725b7c6dedec1f09475229c7d86b7
JennyMendez815 <77779522+JennyMendez815@users.noreply.github.com>
2024-04-13 14:37:53 -0700

    Update CartView.swift

--- CoffeeCrewApp/CartView.swift
+++ CoffeeCrewApp/CartView.swift
@@ -10,12 +10,121 @@ import SwiftUI
 struct CartView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            //Spacer()
+            cartTitle
+            Spacer()
+            cartItems
+            Spacer()
+            cartTotal
+            Spacer()
+            cartCCC
+            Spacer()
+            cartButtons
+            //Spacer()
+            
         }
         .padding()
+        .background(.brown.opacity(0.3))
+    }
+    
+    private var cartTitle: some View {
+        HStack {
+            Spacer()
+            Text("cart")
+            Spacer()
+            
+        }
+        .foregroundColor(Color.black)
+        .padding()
+        .font(.custom("Menlo-Regular", size: 40))
+        //.font(.system(size: 40, weight: .light))
+        .background(.brown.opacity (0.4))
+        .clipShape(RoundedRectangle(cornerRadius: 30))
+    }
+    
+    
+    private var cartItems: some View {
+        HStack {
+            Spacer()
+            VStack {
+                Text("store name #1:\n - list of items\n")
+                Text("store name #2:\n - list of items")
+            }
+            Spacer()
+        }
+        .foregroundColor(Color.black)
+        .padding()
+        .font(.custom("Menlo-Regular", size: 15))
+        //.font(.system(size: 20, weight: .light))
+        .background(.brown.opacity (0.2))
+        .clipShape(RoundedRectangle(cornerRadius: 30))
+    }
+    
+    private var cartTotal: some View {
+        Text("total: $20.00")
+            .foregroundColor(Color.black)
+            .padding()
+            .font(.custom("Menlo-Regular", size: 15))
+            //.font(.system(size: 20, weight: .light))
+            .background(.brown.opacity (0.4))
+            .clipShape(RoundedRectangle(cornerRadius: 30))
+    }
+    
+    private var cartCCC: some View {
+        HStack {
+            Spacer()
+            Text("clear cart")
+                .foregroundColor(Color.black)
+                .padding()
+                .font(.custom("Menlo-Regular", size: 15))
+                //.font(.system(size: 20, weight: .light))
+                .background(.brown.opacity (0.4))
+                .clipShape(RoundedRectangle(cornerRadius: 30))
+            
+            Spacer()
+            
+            Text("checkout")
+                .foregroundColor(Color.black)
+                .padding()
+                .font(.custom("Menlo-Regular", size: 15))
+                //.font(.system(size: 20, weight: .light))
+                .background(.brown.opacity (0.4))
+                .clipShape(RoundedRectangle(cornerRadius: 30))
+            Spacer()
+        }
+    }
+    
+    private var cartButtons: some View {
+        HStack {
+            Spacer()
+            Button(action: {
+                //HomeView
+            }) {
+                Image(systemName: "house.fill")
+            }
+            Spacer()
+            Image(systemName: "cart.fill")
+            Spacer()
+            Button(action: {
+                //SearchView
+            }) {
+                Image(systemName: "magnifyingglass")
+            }
+            Spacer()
+            Button(action: {
+                //ProfileView
+            }) {
+                Image(systemName: "person.fill")
+            }
+            Spacer()
+            
+        }
+        .foregroundColor(Color.black)
+        .padding()
+        .font(.custom("Menlo-Regular", size: 40))
+        //.font(.system(size: 40, weight: .light))
+        .background(.brown.opacity (0.4))
+        .clipShape(RoundedRectangle(cornerRadius: 30))
     }
 }
 
