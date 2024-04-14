30855ccf07dbaae00a7cbb9c5e0bf17cfe3ceb11
JennyMendez815 <77779522+JennyMendez815@users.noreply.github.com>
2024-04-14 14:58:31 -0700

    WelcomeView

--- CoffeeCrewApp/CoffeeCrewAppApp.swift
+++ CoffeeCrewApp/CoffeeCrewAppApp.swift
@@ -11,7 +11,8 @@ import SwiftUI
 struct CoffeeCrewAppApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            //ContentView()
+            WelcomeView()
         }
     }
 }
--- CoffeeCrewApp/WelcomeView.swift
+++ CoffeeCrewApp/WelcomeView.swift
@@ -9,13 +9,47 @@ import SwiftUI
 
 struct WelcomeView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack {
+            Color.brown.opacity(0.3).edgesIgnoringSafeArea(.all)
+            VStack {
+                Spacer()
+                welcomeTitle
+                Spacer()
+                enterButton
+                Spacer()
+            }
+            .padding()
         }
+    }
+    
+    private var welcomeTitle: some View {
+        HStack {
+            Spacer()
+            Text("coffee crew")
+            Spacer()
+            
+        }
+        .foregroundColor(Color.black)
         .padding()
+        .font(.custom("Menlo-Regular", size: 40))
+        .background(.brown.opacity (0.4))
+        .clipShape(RoundedRectangle(cornerRadius: 30))
+    }
+    
+    private var enterButton: some View {
+        HStack {
+            Button(action: {
+                
+            }) {
+                Text("enter")
+                    .padding()
+            }
+        }
+        .foregroundColor(Color.black)
+        .padding()
+        .font(.custom("Menlo-Regular", size: 40))
+        .background(.brown.opacity (0.4))
+        .clipShape(RoundedRectangle(cornerRadius: 30))
     }
 }
 
